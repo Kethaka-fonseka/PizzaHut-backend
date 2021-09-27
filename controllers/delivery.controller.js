4d85bdceacca97e030c2ebf84fc23ccb02030733
Kethaka-fonseka <kethaka.fsk@gmail.com>
2021-09-27 20:44:09 +0530

    Add delivery.controller.js file

---
+++ controllers/delivery.controller.js
@@ -0,0 +1,17 @@
+const Delivery=require("../models/delivery.model");
+
+const addDelivery=async(req,res)=>{
+if(req.body!=null){
+    
+    const delivery=new Delivery(req.body);
+    try{
+const newDelivery=await delivery.save();
+res.status(200).json(newDelivery);
+    }catch(err){
+        res.status(200).json(newDelivery);    
+    }
+}
+}
+module.exports={
+    addDelivery
+}
