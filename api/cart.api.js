bb5c6e30451212f1b9dae6638061900a06f6943b
Kethaka-fonseka <kethaka.fsk@gmail.com>
2021-09-28 18:42:36 +0530

    added new route

--- api/cart.api.js
+++ api/cart.api.js
@@ -11,11 +11,14 @@ router.patch('/update/:id', controller.updateProductQty);
 // Delete product from cart
 router.delete('/delete/:id', controller.removeFromCart);
 
+//Delete all from cart
+router.delete('/delete/all/:user',controller.deleteAllFromCarts);
+
 // Get all cart items
-router.get('/', controller.getAllFromCart);
+router.get('/:user', controller.getAllFromCart);
 
 //Get cart item by id
-router.get('/:id',controller.getCartItemByID)
+router.get('/:id/:size',controller.getCartItemByID)
 
 //Get cart total
 router.get('/total',controller.getTotalPrice)
